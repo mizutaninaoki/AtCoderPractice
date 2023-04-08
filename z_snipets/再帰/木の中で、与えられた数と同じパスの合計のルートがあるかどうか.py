4c9625645af643f03121e026f206b3ee10831f45
mizutaninaoki <applewizardplanet@gmail.com>
2023-04-08 18:20:21 +0900

    add comment

--- z_snipets/再帰/木の中で、与えられた数と同じパスの合計のルートがあるかどうか.py
+++ z_snipets/再帰/木の中で、与えられた数と同じパスの合計のルートがあるかどうか.py
@@ -6,6 +6,8 @@ class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
+        # 子を持たないノード(リーフ)まで到達した時の合計がtargetSumと同じで無いといけない（子ノードを持たないノードに辿り着く前にtargetSumと同じ合計になってもTrueにはならない）
+        # targetSumと現在のノードまでの合計の差が0であれば、その時点までのパスの合計がtargetSumと同じだということ。
         if not root.left and not root.right and root.val == targetSum:
             return True
         return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
