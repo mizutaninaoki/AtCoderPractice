a5a6875936a217245d7d9bb4d36c33f327cb011b
mizutaninaoki <applewizardplanet@gmail.com>
2023-04-22 22:57:38 +0900

    add tips

--- tips.py
+++ tips.py
@@ -18,3 +18,25 @@ print(1 * 2 % 7 * 3 % 7 * 4 % 7 * 5 % 7)
 # 1
 # 他の足し算、引き算も、計算途中の余りをとっても、最終的なあまりは同じ。詳しくは以下参照。
 # see: https://math.nakaken88.com/textbook/cp-remainder/
+
+
+# ------------------------------------------
+# ABC299のD問題
+# インタラクティブな問題の答え方
+# see: https://atcoder.jp/contests/abc299/tasks/abc299_d
+# 他者の解答(二分探索法)
+# ------------------------------------------
+N = int(input())
+mi = 1
+ma = N
+while ma - mi > 1:
+    mid = (mi + ma) // 2
+    print("?", mid)
+    # 必ずwhileの中でprint()の下にインタラクティブな応答を受け付けるinput()を書く！
+    ans = int(input())
+    if ans == 0:
+        mi = mid
+    else:
+        ma = mid
+# 最終的な答えの標準出力はwhileの外に書く！
+print("!", mi)
